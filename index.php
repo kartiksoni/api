00b8d27c67e9aadd8f7e93aa2326b0c47e5f72b0
kartiksoni <kartik.champaneriya@gmail.com>
2016-12-16 15:03:30 +0530

    New commit message

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+//header('Content-type: application/json');
+$k = $_SERVER['REQUEST_METHOD'];
+//$name = isset($_POST['name']) ? mysql_real_escape_string($_POST['name']) : "";
+
+if(isset($k))
+{
+
+$json = array("status" => 1, "msg" => $_POST['name']);
+echo json_encode($json);
+}
+else{
+	$json = array("status" => 0, "msg" => "post value not");
+}
+?>
